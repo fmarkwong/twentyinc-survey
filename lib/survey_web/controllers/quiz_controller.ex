e14867541b4518b4b5d6add4c6d88e8b9bc9f382
Mark Wong <fmarkwong@gmail.com>
2020-05-20 01:46:57 -0700

    End page

--- lib/survey_web/controllers/quiz_controller.ex
+++ lib/survey_web/controllers/quiz_controller.ex
@@ -5,9 +5,10 @@ defmodule SurveyWeb.QuizController do
   alias Survey.Questionnaire.{Answer, Quiz, Question}
   alias Survey.Repo
 
+  @current_user_id 1 #harccoding this.  Normally would get it from session in conn
+
   def index(conn, _params) do
-    current_user_id = 1 #harccoding this.  Normally would get it from session in conn
-    quizzes = Questionnaire.list_quizzes_for_user(current_user_id)
+    quizzes = Questionnaire.list_quizzes_for_user(@current_user_id)
     render(conn, "index.html", quizzes: quizzes)
   end
 
@@ -16,21 +17,24 @@ defmodule SurveyWeb.QuizController do
   end
 
   def next_question(conn, %{"quiz_id" => quiz_id}) do
-    current_user_id = 1 #harccoding this.  Normally would get it from session in conn
-    question = Question.next_unanswered_question_for(Question, String.to_integer(quiz_id), current_user_id) 
+    question = Question.next_unanswered_question_for(Question, String.to_integer(quiz_id), @current_user_id) 
       |> Repo.one()
       |> Repo.preload([:choices, :quiz])
+
+    case question do
+      %Question{} ->
+        render(conn, "next_question.html", question: question) 
+      nil ->
+        render(conn, "end.html") 
+    end
        
 
-    render(conn, "next_question.html", question: question) 
   end
 
   def submit_answer(conn, %{"quiz_id" => quiz_id, "choice_id" => choice_id}) do
-    current_user_id = 1 #harccoding this.  Normally would get it from session in conn
-    
     %Answer{}
     |> Ecto.Changeset.change()
-    |> Ecto.Changeset.put_change(:user_id, current_user_id)
+    |> Ecto.Changeset.put_change(:user_id, @current_user_id)
     |> Answer.changeset(%{choice_id: choice_id})
     |> Repo.insert()
 
